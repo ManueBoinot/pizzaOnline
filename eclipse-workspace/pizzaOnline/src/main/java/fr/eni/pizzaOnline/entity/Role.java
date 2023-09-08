d58a1dd2359fd1dabfcd32fad15476903dacda47
Manue <116074499+ManueBoinot@users.noreply.github.com>
2023-09-08 11:53:33 +0200

    Ajout de Role.java

---
+++ eclipse-workspace/pizzaOnline/src/main/java/fr/eni/pizzaOnline/entity/Role.java
@@ -0,0 +1,31 @@
+package fr.eni.pizzaOnline.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(unique = true, nullable=false)
+	private String libelle;
+
+	public Role(String libelle) {
+		this.libelle = libelle;
+	}
+
+}
